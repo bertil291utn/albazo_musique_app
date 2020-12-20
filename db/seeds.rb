dfd89261674bc5edaaa7af532565fd74230633d7
bertil291utn <bertilt4@gmail.com>
2020-12-20 18:01:34 -0500

    Add seed data to dpa objects

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+dpa1 = Dpa.create! description: "pichincha", hierarchy: 1
+Dpa.create! description: "cayambe", hierarchy: 2, bg_url: "https://", dpa_id: dpa1.id
+Dpa.create! description: "quito", hierarchy: 2, bg_url: "https://", dpa_id: dpa1.id
