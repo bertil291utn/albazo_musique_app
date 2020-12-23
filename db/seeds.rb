9c8a4a4c8a58c4651c329bc31cbe09e60f26438c
bertil291utn <bertilt4@gmail.com>
2020-12-23 16:40:34 -0500

    Add seeds data - Issue: check if have issue at the moment to update an artist

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,18 +5,70 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "faker"
+
 Dpa.delete_all
 Genre.delete_all
+Track.delete_all
+
 dpa1 = Dpa.create! description: "pichincha", hierarchy: 1
 dpa2 = Dpa.create! description: "guayas", hierarchy: 1
-Dpa.create! description: "cayambe", hierarchy: 2, bg_url: "https://", dpa_id: dpa1.id
-Dpa.create! description: "quito", hierarchy: 2, bg_url: "https://", dpa_id: dpa1.id
-Dpa.create! description: "guayaquil", hierarchy: 2, bg_url: "https://", dpa_id: dpa2.id
-Dpa.create! description: "duran", hierarchy: 2, bg_url: "https://", dpa_id: dpa2.id
-genres = %w(rock pop jazz hip blues classic folk heavy_metal)
+Dpa.create! description: "cayambe", hierarchy: 2, bg_url: Faker::LoremFlickr.image, dpa_id: dpa1.id
+Dpa.create! description: "quito", hierarchy: 2, bg_url: Faker::LoremFlickr.image, dpa_id: dpa1.id
+Dpa.create! description: "guayaquil", hierarchy: 2, bg_url: Faker::LoremFlickr.image, dpa_id: dpa2.id
+Dpa.create! description: "duran", hierarchy: 2, bg_url: Faker::LoremFlickr.image, dpa_id: dpa2.id
+
+genres = %w[rock pop jazz hip blues classic folk heavy_metal]
 genres.each do |elem|
-  Genre.create! name: elem, bg_url: "https://"
+  Genre.create! name: elem, bg_url: Faker::LoremFlickr.image
 end
-# puts Genre.all[:id]
-# Arti
-# ArtistGenre.create
+
+tracks_id = %w[5Dqik1P9toCJTLj5rEm78s 5LYJ631w9ps5h9tdvac7yP 2Ch7LmS7r2Gy2kc64wv3Bz 0NdTUS4UiNYCNn5FgVqKQY]
+params = [
+  {
+    name: Faker::Music.band,
+    photourl: Faker::Avatar.image,
+    dpa_id: Dpa.where(hierarchy: 2).first,
+    tracks_attributes: [
+      {
+        name: Faker::Music::Phish.song,
+        spotify_track_id: tracks_id.sample,
+        track_url: "https://",
+        priority: rand(1..5) * 1000,
+        album_bg_url: Faker::LoremFlickr.image,
+      },
+      {
+        name: Faker::Music::Phish.song,
+        spotify_track_id: tracks_id.sample,
+        track_url: "https://",
+        priority: rand(1..5) * 1000,
+        album_bg_url: Faker::LoremFlickr.image,
+      },
+    ],
+  },
+  {
+    name: Faker::Music.band,
+    photourl: Faker::Avatar.image,
+    dpa_id: Dpa.where(hierarchy: 2).first,
+    tracks_attributes: [
+      {
+        name: Faker::Music::Phish.song,
+        spotify_track_id: tracks_id.sample,
+        track_url: "https://",
+        priority: rand(1..5) * 1000,
+        album_bg_url: Faker::LoremFlickr.image,
+      },
+      {
+        name: Faker::Music::Phish.song,
+        spotify_track_id: tracks_id.sample,
+        track_url: "https://",
+        priority: rand(1..5) * 1000,
+        album_bg_url: Faker::LoremFlickr.image,
+      },
+    ],
+  },
+]
+
+params.each do |elem|
+  Artist.create elem
+end
