bcec3a7870f4f8ece371454d9fd3d606aec37638
bertil291utn <bertilt4@gmail.com>
2020-12-23 16:41:26 -0500

    Add accepted_nexted_attributes
    - Issue: check if have issue at the moment to update an artist

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,4 +3,5 @@ class Artist < ApplicationRecord
   has_many :artist_genres
   has_many :genres, through: :artist_genres
   has_many :tracks
+  accepts_nested_attributes_for :tracks
 end
--- app/models/track.rb
+++ app/models/track.rb
@@ -1,3 +1,4 @@
 class Track < ApplicationRecord
   belongs_to :artist
+  validates :artist, presence: true
 end
