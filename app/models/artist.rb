3ae09df83deb3151a14663eec4a32fe0c16be642
bertil291utn <bertilt4@gmail.com>
2020-12-23 15:55:59 -0500

    Migration file - Add relation track and artists

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,4 +2,5 @@ class Artist < ApplicationRecord
   belongs_to :dpa
   has_many :artist_genres
   has_many :genres, through: :artist_genres
+  has_many :tracks
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_22_023410) do
+ActiveRecord::Schema.define(version: 2020_12_23_204912) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,8 +50,21 @@ ActiveRecord::Schema.define(version: 2020_12_22_023410) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "tracks", force: :cascade do |t|
+    t.string "name"
+    t.string "spotify_track_id"
+    t.string "track_url"
+    t.integer "priority"
+    t.string "abum_bg_url"
+    t.bigint "artist_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["artist_id"], name: "index_tracks_on_artist_id"
+  end
+
   add_foreign_key "artist_genres", "artists"
   add_foreign_key "artist_genres", "genres"
   add_foreign_key "artists", "dpas"
   add_foreign_key "dpas", "dpas"
+  add_foreign_key "tracks", "artists"
 end
