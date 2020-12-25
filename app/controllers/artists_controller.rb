08ee70b1c0052abce4513c5fcbde9661727a6784
bertil291utn <bertilt4@gmail.com>
2020-12-25 11:43:56 -0500

    List all arists - Add includes toa void n+1 quieies - Because we need the album background image from track object

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -4,7 +4,7 @@ class ArtistsController < ApplicationController
   # GET /artists
   # GET /artists.json
   def index
-    @artists = Artist.all
+    @artists = Artist.includes :tracks
   end
 
   # GET /artists/1
