9374ef8a0e4516d71fcc41eea4bb59716859fcf7
bertil291utn <bertilt4@gmail.com>
2020-12-25 12:53:02 -0500

    Add genres models - Genres object as root page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   resources :genres
   resources :dpas
   resources :artists
-  root "artists#index"
+  root "genres#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
