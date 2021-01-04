418fe0bd250035b193100aaac9f347d4a5a05bcd
bertil291utn <bertilt4@gmail.com>
2021-01-04 15:18:36 -0500

    Add dpa and track relation - For location (DPA) field on track object - A Dpa have could be on many tracks - A tracks could have only on dpa (location)

--- app/models/dpa.rb
+++ app/models/dpa.rb
@@ -1,4 +1,6 @@
 class Dpa < ApplicationRecord
   belongs_to :dpa, optional: true
   has_many :artists
+  has_many :tracks
+
 end
--- app/models/track.rb
+++ app/models/track.rb
@@ -1,5 +1,6 @@
 class Track < ApplicationRecord
   belongs_to :artist
+  belongs_to :dpa
   validates :artist, presence: true
   has_many :credits_tracks, dependent: :destroy
 end
