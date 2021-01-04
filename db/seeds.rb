33f70325f117f8cb93dc8e7501f863e2e11e2ee6
bertil291utn <bertilt4@gmail.com>
2021-01-04 15:44:34 -0500

    Add new  fields - Add album_name and dpa field on track object - Add spotify_artist_id field on artist object - Add credit_tracks elements

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,12 +9,14 @@ require "faker"
 
 # Delete all records
 ArtistGenre.delete_all
+CreditsTrack.delete_all
 ArtistNetwork.delete_all
 Track.delete_all
 Artist.delete_all
 Dpa.delete_all
 Genre.delete_all
 Network.delete_all
+Rol.delete_all
 
 # DIVISION POLITICO ADMINSITRATIVA (location)
 dpa1 = Dpa.create! description: "pichincha", hierarchy: 1
@@ -61,31 +63,43 @@ networks = [
     url: "https://img.icons8.com/doodle/48/000000/apple-music.png",
   },
 ]
+# ROL
+
+rol = %w(productor escritor interprete derechos escritor label sonido coproductores mezcla)
+
+rol.each do |elem|
+  Rol.create! name: elem
+end
 
 networks.each do |elem|
   Network.create! name: elem[:name], icon_url: elem[:url]
 end
 
 tracks_id = %w[5Dqik1P9toCJTLj5rEm78s 5LYJ631w9ps5h9tdvac7yP 2Ch7LmS7r2Gy2kc64wv3Bz 0NdTUS4UiNYCNn5FgVqKQY]
+artists_id = %w[3yY2gUcIsjMr8hjo51PoJ8 2DaxqgrOhkeH0fpeiQq2f4 0C0XlULifJtAgn6ZNCW2eu 1Xyo4u8uXC1ZmMpatF05PJ]
+dpaTable = Dpa.where(hierarchy: 2).to_a
 params = [
   {
     name: Faker::Music.band,
     photourl: Faker::Avatar.image,
     dpa: Dpa.where(hierarchy: 2).first,
+    spotify_artist_id: artists_id.sample,
     tracks_attributes: [
       {
         name: Faker::Music::Phish.song,
-        spotify_track_id: tracks_id.sample,
         track_url: "https://music-cdn.icons8.com/preview_low/344/9e1b7beb-dd8d-4a99-bde4-6b6e7351ac4f.mp3",
         priority: rand(1..5),
         album_bg_url: Faker::LoremFlickr.image,
+        album_name: Faker::Music::Phish.song,
+        dpa: dpaTable.shuffle!.pop,
       },
       {
         name: Faker::Music::Phish.song,
-        spotify_track_id: tracks_id.sample,
         track_url: "https://music-cdn.icons8.com/preview_low/344/9e1b7beb-dd8d-4a99-bde4-6b6e7351ac4f.mp3",
         priority: rand(1..5),
         album_bg_url: Faker::LoremFlickr.image,
+        album_name: Faker::Music::Phish.song,
+        dpa: dpaTable.shuffle!.pop,
       },
     ],
   },
@@ -93,20 +107,23 @@ params = [
     name: Faker::Music.band,
     photourl: Faker::Avatar.image,
     dpa: Dpa.where(hierarchy: 2).last,
+    spotify_artist_id: artists_id.sample,
     tracks_attributes: [
       {
         name: Faker::Music::Phish.song,
-        spotify_track_id: tracks_id.sample,
         track_url: "https://music-cdn.icons8.com/preview_low/344/9e1b7beb-dd8d-4a99-bde4-6b6e7351ac4f.mp3",
         priority: rand(1..5),
         album_bg_url: Faker::LoremFlickr.image,
+        album_name: Faker::Music::Phish.song,
+        dpa: dpaTable.shuffle!.pop,
       },
       {
         name: Faker::Music::Phish.song,
-        spotify_track_id: tracks_id.sample,
         track_url: "https://music-cdn.icons8.com/preview_low/344/9e1b7beb-dd8d-4a99-bde4-6b6e7351ac4f.mp3",
         priority: rand(1..5),
         album_bg_url: Faker::LoremFlickr.image,
+        album_name: Faker::Music::Phish.song,
+        dpa: dpaTable.shuffle!.pop,
       },
     ],
   },
@@ -116,11 +133,22 @@ params.each do |elem|
   artist = Artist.create! elem
   genreTable = Genre.all.to_a
   networkTable = Network.all.to_a
+
   rand(1..3).times do
     artist.artist_genres.create! genre: genreTable.shuffle!.pop
   end
 
   rand(2..4).times do
     artist.artist_networks.create! social_link: "https://www.facebook.com/", user_name: Faker::Internet.user_name, network: networkTable.shuffle!.pop
+  end
+end
+
+Track.all.each do |elem|
+  artistTable = Artist.all.to_a
+  rolTable = Rol.all.to_a
+
+  rand(3..4).times do
+    elem.credits_tracks.create! artist: [Artist.first, Artist.last].sample, rol: rolTable.shuffle!.pop
+    # elem.credits_tracks.create! artist: artistTable.shuffle!.pop, rol: rolTable.shuffle!.pop
   end
 end
